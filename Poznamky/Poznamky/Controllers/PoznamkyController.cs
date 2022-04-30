faa858581071018033aeecbc97d6457c1c02c22d
Vareel <capousek@spstrutnov.cz>
2022-04-30 12:49:08 +0200

    Dokončení

--- Poznamky/Poznamky/Controllers/PoznamkyController.cs
+++ Poznamky/Poznamky/Controllers/PoznamkyController.cs
@@ -31,7 +31,11 @@ namespace Poznamky.Controllers
         [HttpGet]
         public IActionResult edit()
         {
+
+
             return View();
+
+            
         }
 
         [HttpGet]
@@ -41,7 +45,7 @@ namespace Poznamky.Controllers
         }
 
         [HttpPost]
-        public IActionResult pridat(string nadpis, string text)
+        public IActionResult pridat(string nadpis, string text, string dulezitost)
         {
 
             Models.poznamka nova_poznamka = new Models.poznamka()
@@ -49,17 +53,75 @@ namespace Poznamky.Controllers
                 Nadpis = nadpis,
                 Text = text,
                 CasPridani = DateTime.Now,
-                dulezita_poznamka = false,
+                dulezita_poznamka = !string.IsNullOrEmpty(dulezitost),
                 jmeno = HttpContext.Session.GetString("jmeno")
                 
             };
             Databaze.Add(nova_poznamka);
             Databaze.SaveChanges();
+
             return RedirectToAction("poznamky");
+        }
+
+
+        [HttpGet]
+        public IActionResult smazat() //poznamku
+        {
+            poznamka SameUser = Databaze.Novinky
+           .Where(n => n.jmeno == HttpContext.Session.GetString("jmeno"))
+           .FirstOrDefault();
+
+            poznamka mazany_uzovatel = Databaze.Novinky
+           .Where(n => n.jmeno == HttpContext.Session.GetString("jmeno"))
+           .FirstOrDefault();
+
+
+
+            if (SameUser == null)
+            {
+                return RedirectToAction("Index", "Home");
+            }
+
+            Databaze.Novinky.Remove(SameUser);
+            Databaze.SaveChanges();
+            return RedirectToAction("Index", "Home");
+        }
+
+
+
+        [HttpGet]
+        public IActionResult smazat_ucet()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult smazat_ucet(string heslo) //smazání učtu
+
+        {
+            Models.pripojeni smazany_uzovatel = Databaze.Pristup.Where(U => U.jmeno == HttpContext.Session.GetString("jmeno")).FirstOrDefault();
+
+
+            if (heslo == null || !BCrypt.Net.BCrypt.Verify(heslo, smazany_uzovatel.heslo))
+            {
+                return RedirectToAction("smazat_ucet");
+            }
 
 
             
-        }
+            foreach (poznamka smazanae_poznamky in Databaze.Novinky.Where (P => P.jmeno == HttpContext.Session.GetString("jmeno")))
+            {
+                Databaze.Remove(smazanae_poznamky);
+            }
 
+            Databaze.Remove(smazany_uzovatel);
+            Databaze.SaveChanges();
+            HttpContext.Session.Clear();
+            return RedirectToAction("Index", "Home");
+
+
+
+           
+        }
     }
 }
